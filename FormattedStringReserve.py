7d0be7f0ea956a59c3ded3ce1b5899feada7c66e
Julian Macioce <jjmacioce@hotmail.com>
2023-02-04 14:17:48 -0500

    Update FormattedStringReserve.py

--- FormattedStringReserve.py
+++ FormattedStringReserve.py
@@ -1,6 +1,9 @@
 text = "WOW"
 
 print(f'{text}')
+#print line with fill of character to the left
 print(f'{text:%<20}')
+#print line with fill of character to the right
 print(f'{text:+>20}')
+#print line with fill of character on both sides
 print(f'{text:.^20}')
