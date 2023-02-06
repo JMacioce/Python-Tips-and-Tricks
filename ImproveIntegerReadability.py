3a8ae38858a37f21b2d66ac7fdbaa3044c750f68
Julian Macioce <jjmacioce@hotmail.com>
2023-02-06 14:32:38 -0500

    Update ImproveIntegerReadability.py

--- ImproveIntegerReadability.py
+++ ImproveIntegerReadability.py
@@ -1,7 +1,9 @@
+# Python reads these numbers the same as eachother
 num1 = 1_000_000
 num2 = 1000000
 
 sum = num1 + num2
 print("Two Million -->", sum)
+# This is how to format the print statement to include seperation with a specific character
 print("Two Million -->", f"{sum:,}")
 print("Two Million -->", f"{sum:_}")
