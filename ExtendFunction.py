586be7f0209f1032127622628769c226e8e72401
Julian Macioce <jjmacioce@hotmail.com>
2023-04-23 17:00:34 -0400

    Update ExtendFunction.py

--- ExtendFunction.py
+++ ExtendFunction.py
@@ -1,7 +1,7 @@
 # What our player currently holds
 Inventory = ["Wooden Sword", "Magic Hat", "Flame Orb"]
 
-# What are player has stumbled upon and wishes to add to their inventory
+# What our player has stumbled upon and wishes to add to their inventory
 Chest = ["Health Potion", "Stamina Potion", "Map of Floor"]
 
 # We could do this
